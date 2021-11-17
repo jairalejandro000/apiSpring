3e1e1ce10bc68154b2f7df77edcca468eb210c8c
jair alejandro <jairalejandro32@outlook.com>
2021-11-17 15:29:34 -0600

    Subida

---
+++ src/main/java/com/example/apiSpring/ApiSpringApplication.java
@@ -0,0 +1,14 @@
+package com.example.apiSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class ApiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSpringApplication.class, args);
+	}
+
+}
